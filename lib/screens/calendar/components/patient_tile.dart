1bd4df174f6fa0909f91a11427d5428592f3e24f
Me5a2000 <58401842+MohamedElmeligy@users.noreply.github.com>
2021-10-16 00:06:15 +0200

    task 1 assigned

---
+++ lib/screens/calendar/components/patient_tile.dart
@@ -0,0 +1,18 @@
+//Omnia
+import 'package:flutter/material.dart';
+
+class PatientTile extends StatefulWidget {
+  const PatientTile({ Key? key }) : super(key: key);
+
+  @override
+  _PatientTileState createState() => _PatientTileState();
+}
+
+class _PatientTileState extends State<PatientTile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ lib/screens/calendar/components/slot_tile.dart
@@ -0,0 +1,16 @@
+//Menna & Youmna
+import 'package:flutter/material.dart';
+
+class SlotTile extends StatefulWidget {
+  const SlotTile({Key? key}) : super(key: key);
+
+  @override
+  _SlotTileState createState() => _SlotTileState();
+}
+
+class _SlotTileState extends State<SlotTile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
