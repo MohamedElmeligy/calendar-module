92860640c915f87121d52d81db857e457e6ca6c9
Marwan Khaled <83166836+fluttermaster@users.noreply.github.com>
2021-10-16 22:10:31 +0200

    calendar prototype

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'calendar/calendar.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -12,7 +13,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("Home Screen"),
+        child: CalendarScreen(),
       ),
     );
   }
