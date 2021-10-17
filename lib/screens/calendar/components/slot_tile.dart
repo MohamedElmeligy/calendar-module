ad991704a58aab36696869d833fa31afbb83cf3e
mennaabdelsattar <92600902+mennaabdelsattar@users.noreply.github.com>
2021-10-17 16:22:16 +0200

    slot_tile

--- lib/screens/calendar/components/slot_tile.dart
+++ lib/screens/calendar/components/slot_tile.dart
@@ -1,5 +1,6 @@
 //Menna & Youmna
 import 'package:flutter/material.dart';
+import 'package:calendar_module/main.dart';
 
 class SlotTile extends StatefulWidget {
   const SlotTile({Key? key}) : super(key: key);
@@ -9,8 +10,59 @@ class SlotTile extends StatefulWidget {
 }
 
 class _SlotTileState extends State<SlotTile> {
+  List<String> patients = ["yomna"];
+
+  void addPatient() {
+    patients.add("patient");
+    setState(() {
+      
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      width: 150,
+      height: 100,
+      child: patients.isEmpty
+          ? Center(
+              child: IconButton(
+                onPressed: addPatient,
+                icon: const Icon(
+                  Icons.add_circle_rounded,
+                  color: Colors.pink,
+                  size: 50,
+                ),
+              ),
+            )
+          : ListView.builder(
+              shrinkWrap: true,
+              itemCount: patients.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Row(
+                  children: [
+                    Text(patients[index]),
+                    index == 0
+                        ? IconButton(
+                            onPressed: addPatient,
+                            icon: const Icon(
+                              Icons.add_box_rounded,
+                              color: Colors.black,
+                              size: 45,
+                            ),
+                          )
+                        : Container()
+                  ],
+                );
+              }),
+/*
+ Align( alignment: Alignment.bottomRight,
+       child: IconButton(
+         onPressed: addPatient,
+          icon: const Icon(Icons.add_box_rounded, color: Colors.pink, size: 45, ),
+          ) ,
+       )
+    */
+    );
   }
 }
